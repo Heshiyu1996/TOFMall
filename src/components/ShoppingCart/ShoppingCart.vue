43eb5ba8823d7da05ad1c5b408065b2aaf2dc334
你的宇哥哥 <450979309@qq.com>
2017-07-16 09:11:25 +0800

    购物车v0.2

--- src/components/ShoppingCart/ShoppingCart.vue
+++ src/components/ShoppingCart/ShoppingCart.vue
@@ -10,7 +10,7 @@
           <el-checkbox
           :indeterminate="isIndeterminate"
           v-model="checkAll"
-          @change="handleCheckAllChange">全选
+          @change="handleCheckAllChange">
           </el-checkbox>
         </div>
         <div class="css-title-name">商品信息</div>
@@ -22,13 +22,12 @@
     <div class="clearfix"></div>
 
     <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
-      <el-checkbox :key="cities[0]"  :label="cities[0]" ></el-checkbox>
-      <el-checkbox :key="cities[1]"  :label="cities[1]" ></el-checkbox>
-    </el-checkbox-group>
-
       <section>
         <div class="css-body">
           <div class="css-body-item">
+            <div class="css-body-item-select">
+              <el-checkbox :key="cities[0]"  :label="cities[0]" ></el-checkbox>
+            </div>
             <div class="css-body-item-img" style="">
               <img src="./../../assets/img/car9.jpg" />
             </div>
@@ -44,19 +43,22 @@
       <section>
         <div class="css-body">
           <div class="css-body-item">
+            <div class="css-body-item-select">
+              <el-checkbox :key="cities[1]"  :label="cities[1]" ></el-checkbox>
+            </div>
             <div class="css-body-item-img" style="">
                 <img src="./../../assets/img/car9.jpg" />
             </div>
             <div class="css-body-item-name">汽车</div>
             <div class="css-body-item-price">汽车</div>
             <div class="css-body-item-count">
-              <el-input-number size="small" v-model="count" @change="handleChange" :min="1" :max="99"></el-input-number>
+              <el-input-number size="small" v-model="count2" @change="handleChange" :min="1" :max="99"></el-input-number>
             </div>
             <div class="css-body-item-sum">¥ 29.99</div>
           </div>
         </div>
       </section>
-
+    </el-checkbox-group>
     <section>
       <div class="css-footer">
         <div class="css-footer-sum">合计 ： <span class="css-footer-sum-num">¥ 99999.00</span></div>
@@ -67,15 +69,16 @@
 </template>
 
 <script>
-const cityOptions = ['上海', '北京', '广州', '深圳'];
+// const cityOptions = ;
 
 export default {
   data () {
     return {
       count: 2,
+      count2: 3,
       checked: false,
 
-      cities: cityOptions,
+      cities: ['上海', '北京'],
 
       checkedCities: [],
       checkAll: false,
@@ -87,7 +90,7 @@ export default {
       console.log(value);
     },
     handleCheckAllChange(event) {
-      this.checkedCities = event.target.checked ? cityOptions : [];
+      this.checkedCities = event.target.checked ?  ['上海', '北京'] : [];
       this.isIndeterminate = false;
     },
     handleCheckedCitiesChange(value) {
@@ -101,42 +104,65 @@ export default {
 
 <style scoped lang="scss">
 
-  .css-footer {
-    margin-top: 10px;
-    float: right;
-    .css-footer-sum {
-      padding-top: 5px;
-      width: 200px;
-      height:35px;
-      font-size: 22px;
-      margin-right: 20px;
-      float:left;
-      .css-footer-sum-num {
-        margin-left: 10px;
-        color: red;
-      }
-    }
-    .css-footer-btn {
-      float:left;
-      background-color: green;
-    }
-  }
   .css-top {
     float:left;
     font-size:20px;
     font-weight: bold;
     height:40px;
   }
+
+  .css-title {
+    width:100%;
+    text-align: right;
+    margin-top: 5px;
+    .css-title-select {
+      padding-right: 15px;
+      background-color: red;
+      float:left;
+      width:5%;
+    }
+    .css-title-all {
+      padding-right: 47px;
+      float:left;
+      width:10%;
+    }
+    .css-title-name {
+      padding-right: 50px;
+      float:left;
+      width:20%;
+    }
+    .css-title-price {
+      padding-right: 5px;
+      float:left;
+      width:20%;
+    }
+    .css-title-count {
+      float:left;
+      width:20%;
+    }
+    .css-title-sum {
+      padding-right: 90px;
+      float:left;
+      width:30%;
+    }
+  }
+
   .css-body {
     .css-body-item {
       margin-top: 5px;
       border-bottom: 1px solid black;
       width:860px;
       height:100px;
-      padding: 10px 30px 10px 30px;
+      padding: 10px 30px 10px 20px;
       text-align: right;
+      .css-body-item-select {
+        float: left;
+        width:5%;
+        height:70px;
+        padding: 20px 30px 0px 0px;
+      }
       .css-body-item-img {
-        width: 70px;
+        width: 100px;
         height: 70px;
         float: left;
         img {
@@ -148,25 +174,27 @@ export default {
         float: left;
         width:19.7%;
         height:70px;
-        padding: 20px 30px 0px 0px;
+        text-align: center;
+        padding: 20px 0px 0px 0px;
       }
       .css-body-item-price {
         float: left;
         width:21.6%;
         height:70px;
-        padding: 20px 55px 0px 0px;
+        text-align: center;
+        padding: 20px 0px 0px 0px;
       }
       .css-body-item-count {
         float: left;
         width:21.7%;
         height:70px;
-        padding: 20px 0px 0px 0px;
+        padding: 20px 10px 0px 0px;
       }
       .css-body-item-sum {
         overflow:hidden;
         line-height: 70px;
         float: left;
-        width:28%;
+        width:15%;
         height:70px;
         color: red;
         text-align: center;
@@ -174,35 +202,25 @@ export default {
       }
     }
   }
-  .css-title {
-    width:100%;
-    text-align: right;
-    margin-top: 5px;
-    .css-title-all {
-      padding-right: 15px;
-      float:left;
-      width:15%;
-    }
-    .css-title-name {
-      padding-right: 15px;
-      float:left;
-      width:15%;
-    }
-    .css-title-price {
-      padding-right: 55px;
-      float:left;
-      width:20%;
-    }
-    .css-title-count {
-      padding-right: 55px;
-      float:left;
-      width:20%;
-    }
-    .css-title-sum {
-      padding-right: 90px;
-      float:left;
-      width:30%;
-    }
-  }
 
+    .css-footer {
+      margin-top: 10px;
+      float: right;
+      .css-footer-sum {
+        padding-top: 5px;
+        width: 200px;
+        height:35px;
+        font-size: 22px;
+        margin-right: 20px;
+        float:left;
+        .css-footer-sum-num {
+          margin-left: 10px;
+          color: red;
+        }
+      }
+      .css-footer-btn {
+        float:left;
+        background-color: green;
+      }
+    }
 </style>
